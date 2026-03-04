afabf22a16945e03cef1f471db12a1be50126735
Lucas Wheeler <lw80982p@pace.edu>
2026-03-04 15:43:12 -0500

    init

---
+++ grocery-list/src/components/GroceryList.jsx
@@ -0,0 +1,37 @@
+﻿import {useState} from 'react'
+
+import Form from "react-bootstrap/Form";
+import CloseButton from "react-bootstrap/CloseButton";
+import InputGroup from "react-bootstrap/InputGroup";
+
+const GroceryList = ({ initItems }) => {
+    const [items, setItems] = useState(initItems);
+
+    const addItem = () => {
+        const html = document.getElementById("newItemID");
+        if (html.value) {
+            setItems([...items, html.value]);
+            html.value = "";
+        }
+    }
+
+    const delItem = index => {
+        items.splice(index, 1);
+        setItems([...items]);
+    }
+
+    return <>
+        <InputGroup className="w-50 m-5">
+            <Form.Control type="text" id="newItemID" placeholder="New Item"/>
+            <InputGroup.Text className="btn  btn-primary" onClick={addItem}>Add</InputGroup.Text>
+        </InputGroup>
+        {items.map(((itm, idx) =>
+                <InputGroup className="w-50 m-5 border rounded">
+                    <Form.Label className="mx-2" column="lg">{itm}</Form.Label>
+                    <CloseButton className="m-auto p-2" onClick={() => delItem(idx)}/>
+                </InputGroup>
+        ))}
+    </>
+}
+
+export default GroceryList;
---
+++ grocery-list/src/main.jsx
@@ -0,0 +1,15 @@
+﻿import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+
+import GroceryList from './components/GroceryList.jsx'
+import Container from "react-bootstrap/Container";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <Container className="border m-auto ">
+            <GroceryList initItems={["bacon", "broccoli", "carrots"]}/>
+        </Container>
+    </StrictMode>,
+)
